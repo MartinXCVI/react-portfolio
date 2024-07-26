45da6b85196613c3d1e11ed95d5810ae935bea18
MartinXCVI <mdzg96@gmail.com>
2024-07-26 01:49:38 -0300

    added responsiveness and more styling to portfolio section and about section

--- src/components/Portfolio/WordpressProjects.jsx
+++ src/components/Portfolio/WordpressProjects.jsx
@@ -11,13 +11,13 @@ const WordpressProjects = () => {
           page={"https://personal-website.free.nf/"}
         />
         <WPCard 
-          title="Wordpress Blog"
+          title="Travel Blog"
           img="/images/project-image.jpg"
           description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
           page={"https://wordpress-blog.free.nf/"}
         />
         <WPCard 
-          title="Wordpress Site?"
+          title="...WooCommerce?"
           img="/images/project-image.jpg"
           description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
           page={"https://personal-website.free.nf/"}
