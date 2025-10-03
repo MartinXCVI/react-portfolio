ed4fcca2a3aa3f599c433c0c7d13b3027fcb861a
MartinXCVI <mdzg96@gmail.com>
2025-10-02 21:34:02 -0300

    Update in frontend projects section: Removing 3 that are not so necessary

--- src/components/Portfolio/WebsitesProjects.tsx
+++ src/components/Portfolio/WebsitesProjects.tsx
@@ -16,26 +16,6 @@ const WebsitesProjects = (): JSX.Element => {
         repository={"https://github.com/MartinXCVI/bakery"}
       />
       <ProjectCard 
-        aosAnim="zoom-out-up" 
-        aosDuration="1500"
-        aosDelay="150"
-        title="PelículasCAC"
-        img="/images/covers/peliculas-cac-cover.webp"
-        description="Frontend project based on a films & cartoons streaming platform for the 'Codo a Codo' bootcamp. Developed in a group of 4 under the scrum modality. Responsive, 'AOS' scroll library, forms validation, and APIs consumption."
-        page={"https://martinxcvi.github.io/PeliculasCAC/"}
-        repository={"https://github.com/MartinXCVI/PeliculasCAC"}
-      />
-      <ProjectCard
-        aosAnim="zoom-out-up" 
-        aosDuration="1500"
-        aosDelay="300"
-        title="CV Website"
-        img="/images/covers/cv-website-cover.webp"
-        description="Minimalist yet elegant website template for a curriculum vitae. Developed with the Astro Framework, JavaScript DOM manipulation, clean design, Web3Forms emails, PDF download button, and 'AOS' animations library."
-        page={"https://cv-website-rd.vercel.app/"}
-        repository={"https://github.com/MartinXCVI/cv-website"}
-      />
-      <ProjectCard 
         aosAnim="zoom-out-down" 
         aosDuration="1500"
         aosDelay="450"
@@ -44,16 +24,6 @@ const WebsitesProjects = (): JSX.Element => {
         description="Clean and beautiful design for a landscapist/landscaper, gardener, or arborist website alternatively. Developed with the Astro Framework, TypeScript, AOS libraries and SCSS. Contact form functionality with Web3Forms."
         page={"https://landscapist.vercel.app/"}
         repository={"https://github.com/MartinXCVI/landscapist"}
-      />
-      <ProjectCard
-        aosAnim="zoom-out-down" 
-        aosDuration="1500" 
-        aosDelay="600"
-        title="Dark Ambient Form"
-        img="/images/covers/dark-ambient-form-cover.webp"
-        description="A lovecraftian subscription form with dark tonalities and interestingly entertaining moving green spheres. Small yet curiously complex project in its programmatically sense. Developed with React, Vite and SASS preprocessor."
-        page={"https://dark-ambient-formulary.vercel.app/"}
-        repository={"https://github.com/MartinXCVI/Dark-Ambient-Formulary"}
       />
       <ProjectCard
         aosAnim="zoom-out-down" 
